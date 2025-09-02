459ebd91af9f0fdadb3d93e399e772e5da3b4b58
Lokesh A <lokegowda143@gmail.com>
2025-09-02 09:18:26 +0530

    Update 3d.py

--- MLlab/3d.py
+++ MLlab/3d.py
@@ -1,29 +1,12 @@
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D  
-
-
-df = pd.read_csv('toyotacorola.csv')
-
-
-x = df['KM']
-y = df['Doors']
-z = df['Price']
-
-fig = plt.figure(figsize=(10, 7))
-ax = fig.add_subplot(111, projection='3d')
-
-
-surf = ax.plot_trisurf(x, y, z, cmap='jet', edgecolor='none')
-
-
-ax.set_xlabel('KM Driven')
-ax.set_ylabel('Number of Doors')
-ax.set_zlabel('Price')
-
-
-fig.colorbar(surf, ax=ax, shrink=0.5, aspect=10)
-
-
-plt.title('3D Price Visualization (KM vs Doors vs Price)')
-plt.show()
+def plot_3d():
+    x = np.linspace(-5, 5, 50)
+    y = np.linspace(-5, 5, 50)
+    X, Y = np.meshgrid(x, y)
+    Z = np.sin(np.sqrt(X**2 + Y**2))
+    ax = plt.figure().add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, cmap='viridis')
+    ax.set_title("3D Surface Plot")
+    plt.show()
+plot_3d()
